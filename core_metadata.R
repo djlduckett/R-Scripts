8f660fcf89c46d96a59d3c129907a5918e078d04
Drew Duckett <duckettdj@g.cofc.edu>
2017-02-08 13:42:47 -0600

    Looped over all files in directory
    Looped core_metadata over all raw files in directory. Now returns a
    dataframe for all samples in directory. Also fixed a glitch that would
    cause code to error out if a file only contained one sample

--- core_metadata.R
+++ core_metadata.R
@@ -1,39 +1,52 @@
 test_rwl <- read.rwl("007.1.00")
 
 
-core_metadata <- function(file_name, project, location, PI, label){
+
+core_metadata <- function(project, location, PI, label){
   
   #Load required libraries
   library(dplR)
   
-  rwl_df <- read.rwl(file_name) #read in data
+  #get all raw file names to run
+  dat_files <- Sys.glob("*") #get all file names in directory
+  rem_files <- dat_files[grep("*.xlsx$", dat_files)] #get file names to remove
+  dat_files <- dat_files[!dat_files %in% rem_files] #remove those files with .xlsx
   
-  #create data frame for data
-  df_name <- data.frame(stringsAsFactors = FALSE)
+  #create df for all data
+  df_final <- data.frame(stringsAsFactors = FALSE)
   #assign(label, df_name)
   
-  i = 1
-  for (i in 1:ncol(rwl_df)){ #for each sample
+  f = 1
+  for (y in 1:length(dat_files)){
     
-    #get sample name
-    samp_name <- names(rwl_df[i])
+    rwl_df <- read.rwl(dat_files[f]) #read in data
     
-    #get species name
-    species_name <- gsub("\\d", "", samp_name) #replace digits
+    i = 1
+    for (z in 1:ncol(rwl_df)){ #for each sample
+      
+      #get sample name
+      samp_name <- names(rwl_df[i])
+      
+      #get species name
+      species_name <- gsub("\\d", "", samp_name) #replace digits
+      
+      #get start and end years
+      no_na <- which(!is.na(rwl_df[,i]), arr.ind = TRUE) #get indices of rows without NA
+      begin <- row.names(rwl_df)[no_na[1]] #get year of first datapoint
+      end <- row.names(rwl_df)[no_na[length(no_na)]] #get year of last datapoint
+      
+      #combine data in single df
+      data_list <- list(project, location, PI, samp_name, species_name, begin, end) #combine data in list
+      df_final <- rbind(df_final, data_list, stringsAsFactors = FALSE) #add list as row
+      
+      i = i + 1
+    }
     
-    #get start and end years
-    no_na <- which(!is.na(rwl_df[,i]), arr.ind = TRUE) #get indices of rows without NA
-    begin <- row.names(rwl_df[no_na[1],]) #get year of first datapoint
-    end <- row.names(rwl_df[no_na[length(no_na)],]) #get year of last datapoint
-    
-    #combine data in single df
-    data_list <- list(project, location, PI, samp_name, species_name, begin, end) #combine data in list
-    df_name <- rbind(df_name, data_list, stringsAsFactors = FALSE) #add list as row
-    
-    i = i + 1
+    f = f + 1
   }
-  colnames(df_name) <- c("Project", "Location", "PI", "Sample", "Species", "YR_Start", "YR_End") #rename columns
-  return(df_name)
+  
+  colnames(df_final) <- c("Project", "Location", "PI", "Sample", "Species", "YR_Start", "YR_End") #rename columns
+  return(df_final)
 }
 
 
@@ -42,9 +55,12 @@ dat_files <- Sys.glob("*") #get all file names in directory
 rem_files <- dat_files[grep("*.xlsx$", dat_files)] #get file names to remove
 dat_files <- dat_files[!dat_files %in% rem_files] #remove those files with .xlsx
 
+#create df for all data
+df_final <- data.frame(stringsAsFactors = FALSE)
 
 
 
-core_metadata("007.1.00", "Project", "Location", "PI", "label")
+
+coredf_test <- core_metadata("INAI", "Ryerson", "Bob Fahey", "label")
 
 
