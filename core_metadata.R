1df75c00f9f41ee68a04c4dc85d5d024b386b356
Drew Duckett <duckettdj@g.cofc.edu>
2017-02-08 14:31:15 -0600

    Added line to write data to csv
    Added code to write all the data to a csv in the working directory. Also
    added more notes

--- core_metadata.R
+++ core_metadata.R
@@ -1,11 +1,55 @@
-test_rwl <- read.rwl("007.1.00")
+#------------------------------------
+# Script Information
+#------------------------------------
+# Purpose: Extra metadata from raw tree core measurement files 
+# Creator: Drew Duckett, 08 February 2017
+# Contact: duckettdj@g.cofc.edu
+#------------------------------------
+
+#------------------------------------
+# Description
+#------------------------------------
+# Defines a function (core_metadata) to extract metadata (saple names, species, start and end years)
+#   from decadal tree core measurement files. Then runn the function with the appropriate user input and
+#   write data to a csv file
+
+# core_meatdata input:
+# 1. Project - the name of the project (string)
+# 2. Location - where the cores were taken (string)
+# 3. PI - who took the cores (string)
+# 4. A name to store the dataframe as
+
+# core_metadata output:
+# dataframe with one row for each sample
+
+# additional script input:
+# 1. A file name for the csv
+
+# additional script output:
+# A csv file in the current directory containing all the meatadata for that directory
+
+# Note: check the dataframe before writing to a csv. Different data files from different PI's may use different 
+#   naming conventions, which may require some formatting
+
+#------------------------------------
+# Workflow Overview
+#------------------------------------
+# core_meatadata:
+# 1) Get names for all filenames in directory that are not excel spreadsheets
+# 2) Import data using dplR package to convert from decadal format to dataframe
+# 3) Isolate metadata and write to dataframe
+
+#------------------------------------
 
 
 
-core_metadata <- function(project, location, PI, label){
+setwd() #set wd to location of files
+
+core_metadata <- function(project, location, PI){
   
   #Load required libraries
   library(dplR)
+  library(utils)
   
   #get all raw file names to run
   dat_files <- Sys.glob("*") #get all file names in directory
@@ -36,7 +80,7 @@ core_metadata <- function(project, location, PI, label){
       end <- row.names(rwl_df)[no_na[length(no_na)]] #get year of last datapoint
       
       #combine data in single df
-      data_list <- list(project, location, PI, samp_name, species_name, begin, end) #combine data in list
+      data_list <- list(project, location, PI, samp_name, species_name, begin, end, dat_files[f]) #combine data in list
       df_final <- rbind(df_final, data_list, stringsAsFactors = FALSE) #add list as row
       
       i = i + 1
@@ -45,22 +89,17 @@ core_metadata <- function(project, location, PI, label){
     f = f + 1
   }
   
-  colnames(df_final) <- c("Project", "Location", "PI", "Sample", "Species", "YR_Start", "YR_End") #rename columns
+  colnames(df_final) <- c("Project", "Location", "PI", "Sample", "Species", "YR_Start", "YR_End", "File_name") #rename columns
   return(df_final)
 }
 
 
-#get all raw file names to run
-dat_files <- Sys.glob("*") #get all file names in directory
-rem_files <- dat_files[grep("*.xlsx$", dat_files)] #get file names to remove
-dat_files <- dat_files[!dat_files %in% rem_files] #remove those files with .xlsx
+# Run as: input_name <- core_metadata("Project_Name", "Location_Name", "PI_Name")
+#         write.csv(input_name, file = "input.csv")
 
-#create df for all data
-df_final <- data.frame(stringsAsFactors = FALSE)
+Ryerson_Flatwoods <- core_metadata("INAI", "Ryerson", "Bob Fahey") #run function
 
+# Check dataframe
 
-
-
-coredf_test <- core_metadata("INAI", "Ryerson", "Bob Fahey", "label")
-
+write.csv(Ryerson_Flatwoods, file = "Ryerson_Flatwoods.csv") #write df to csv
 
